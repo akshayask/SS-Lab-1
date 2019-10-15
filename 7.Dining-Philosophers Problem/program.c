a60f04ebe668b9a302ca2f3c8452339431a8eaa6
AKSHAYA SK <44637906+akshayask@users.noreply.github.com>
2019-10-15 18:51:10 +0530

    Updated version
    4

--- 7.Dining-Philosophers Problem/program.c
+++ 7.Dining-Philosophers Problem/program.c
@@ -12,7 +12,6 @@ sem_t mutex;
 sem_t S[N];
 void * philospher(void * num);
 void take_fork(int);
-void put_fork(int);
 void test(int);
 int state[N];
 int phil_num[N] = {
@@ -40,7 +39,7 @@ void * philospher(void * num) {
         sleep(1);
         take_fork( * i);
         sleep(0);
-        put_fork( * i);
+        
     }
 }
 void take_fork(int ph_num) {
@@ -65,12 +64,4 @@ void test(int ph_num) //check for the availability of forks
             sem_post( & S[ph_num]);
         }
     }
-    void put_fork(int ph_num) {
-        sem_wait( & mutex);
-        state[ph_num] = THINKING;
-        printf("Philosopher %d putting fork %d and %d down\n", ph_num + 1, LEFT + 1, ph_num + 1); //after finished eating , both forks put down
-        printf("Philosopher %d is thinking\n", ph_num + 1);
-        test(LEFT);
-        test(RIGHT);
-        sem_post( & mutex);
-    }
+    
