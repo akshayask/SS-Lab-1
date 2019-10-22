be6a64330a117265d80d342750cc82b4e29da33a
thasna2122 <56201386+thasna2122@users.noreply.github.com>
2019-10-22 19:26:30 +0530

    Update program.c

--- 5.Bankers Algorithm/program.c
+++ 5.Bankers Algorithm/program.c
@@ -1,111 +1,168 @@
-
-
-
-#include <stdio.h>
-struct file
+#include<stdio.h>
+int m, n;
+int safe ( int a[10][10],int  ma[10][10],int  av[10],int ne[10][10])
 {
-        int all[10];
-        int max[10];
-        int need[10];
-        int flag;
-} f[10];
-void main()
-{
-        int fl;
-        int i, j, k, p, b, n, r, g, cnt=0, id, newr;
-        int avail[10],seq[10];
-        printf("\nEnter number of processes: ");
-        scanf("%d",&n);
-        printf("Enter number of resources: ");
-        scanf("%d",&r);
-        for(i=0; i<n; i++) //Entering details of all processes
-        {
-                printf("Enter details for P%d",i);
-                printf("\nEnter allocation\t: \t");
-                for(j=0; j<r; j++)
-                        scanf("%d",&f[i].all[j]);
-                printf("Enter Max\t\t: \t");
-                for(j=0; j<r; j++)
-                        scanf("%d",&f[i].max[j]);
-                f[i].flag=0;
+  int k,i,j,y,f[n],ans[n],ind=0,d,e,flag,t=0;
+  
+  for(t=0;t<n;t++)
+  {
+      f[t]=0;//finish=0 for all process.
+     
+       printf("%d\n",f[t]);
+  }
+    for(k=0;k<n;k++)
+    {
+        for(e=0;e<n;e++)
+          {  
+         if (f[e] == 0)
+	    {
+	        flag = 0;
+    	    for (j = 0; j < m; j++)
+    	  {
+	      if (ne[e][j] > av[j])
+		
+		    flag = 1;
+		    break;
+		  }
+	       if (flag == 0)
+	      {
+	      f[e] = 1;
+
+	      for (j = 0; j < m; j++)
+	   	  
+		        av[j] += a[e][j];
+		  
+		  
+		  ans[ind++]=e;//the safety sequence is arranged in to array named ans[].
+	      }
+	    }
         }
-        printf("\nEnter Available Resources\t: \t");
-        for(i=0; i<r; i++)
-                scanf("%d",&avail[i]);
-        printf("\nEnter New Request Details : "); //New request
-        printf("\nEnter pid \t : \t");
-        scanf("%d",&id);
-        printf("Enter Request for Resources \t: \t");
-        for(i=0; i<r; i++)
+    }
+    
+    for(y=0;y<n;y++)
         {
-                scanf("%d",&newr);
-                f[id].all[i] += newr; //allocation = allocation + new request
-                avail[i]=avail[i]-newr;// available = available - new request
+            if(f[y]!=1)//process is not completed.
+                return 0;//request is not granted.
+            else
+                {printf("p[%d]->",ans[y]);
+                 }
+	  
         }
-        for(i=0; i<n; i++)
-        {
-                for(j=0; j<r; j++)
-                {
-                        f[i].need[j] = f[i].max[j]-f[i].all[j]; //need = max - allocation
-                        if(f[i].need[j]<0)
-                                f[i].need[j]=0;
-                }
-        }
-        cnt=0;
-        fl=0;
-        while(cnt!=n)
-        {
-                g=0;
-                for(j=0; j<n; j++)
-                {
-                        if(f[j].flag==0)
-                        {
-                                b=0;
-                                for(p=0; p<r; p++)
-                                {
-                                        if(avail[p]>=f[j].need[p]) //check whether need less than availability (need <= work)
-                                                b=b+1;
-                                        else
-                                                b=b-1;
-                                }
-                                if(b==r)
-                                {
-                                        printf("\nP%d is visited",j); //Printing the visited processes
-                                        seq[fl++]=j;
-                                        f[j].flag=1;
-                                        for(k=0; k<r; k++)
-                                                avail[k]=avail[k]+f[j].all[k]; // available = available + allocation
-                                        cnt=cnt+1;
-                                        printf("(");
-                                        for(k=0; k<r; k++)
-                                                printf(" %d",avail[k]);
-                                        printf(")");
-                                        g=1;
-                                }
-                        }
-                }
-                if(g==0) //Unsafe state
-                {
-                        printf("\n REQUEST NOT GRANTED- DEADLOCK OCCURRED");
-                        printf("\n SYSTEM IS IN UNSAFE STATE");
-                        goto y;
-                }
-        }
-        printf("\nSYSTEM IS IN SAFE STATE"); //Safe state
-        printf("\nThe Safe Sequence is : (");
-        for(i=0; i<fl; i++)
-                printf("P%d ",seq[i]);
-        printf(")\n");
-y: printf("\nProcess\t\tAllocation\tMax\t\t   Need\n");
-        for(i=0; i<n; i++)
-        {
-                printf("P%d\t",i);
-                for(j=0; j<r; j++)
-                        printf(" %d\t",f[i].all[j]);
-                for(j=0; j<r; j++)
-                        printf(" %d\t",f[i].max[j]);
-                for(j=0; j<r; j++)
-                        printf(" %d\t",f[i].need[j]);
-                printf("\n");
-        }
+        return 1;
 }
+
+void main ()
+{
+  int i, j,a[10][10],f, ma[10][10], av[10], ne[10][10],k=0,d=0,r[10],w;
+  printf ("enter no of p\n");
+  scanf ("%d", &n);
+  printf ("enter no of r\n");
+  scanf ("%d", &m);
+   printf ("allocation vector\n");
+  for (i = 0; i < n; i++)
+    {
+      
+
+     
+      for (j = 0; j < m; j++)
+	{
+
+	  scanf ("%d", &a[i][j]);
+	}
+    }
+    printf ("maximum vector\n");
+    for (i = 0; i < n; i++)
+    {
+      
+      for (j = 0; j < m; j++)
+	{
+
+	  scanf ("%d", &ma[i][j]);
+	}
+    }
+     
+
+
+    
+     printf ("available") ;
+      for (j = 0; j < m; j++)
+	{
+
+	  scanf ("%d", &av[j]);
+	}
+  printf ("need\n");
+  for (k=0; k<n; k++)
+    {
+      for (j = 0; j<m; j++)
+	{
+	    ne[k][j]=ma[k][j] - a[k][j];//need=maximum-allocation
+	  printf ("%d",ne[k][j] );
+	}
+	printf("\n");
+    }
+    printf("process\tallocation\tmaximum\tneed\n");
+  for(d=0;d<n;d++)
+  {     
+      for(j=0;j<m;j++)
+      {
+           printf("%d",a[d][j]);
+      }printf("\t");
+      for(j=0;j<m;j++)
+      {
+           printf("%d",ma[d][j]);
+      }printf("\t");
+      for(j=0;j<m;j++)
+      {
+           printf("%d",ne[d][j]);
+      }printf("\n");
+  }
+   printf("available\n");
+      for(j=0;j<m;j++)
+      {
+           printf("%d",av[j]);
+      }
+      printf("\n");
+      printf(" process number\n");//enter number of process to request resources
+      scanf("%d",&w);
+      printf("enter request\n");//resource request
+      for(j=0;j<m;j++)
+       {
+           scanf("%d",&r[j]);
+       }
+       for(k=0;k<n;k++)
+       {    if(k==w)
+            {
+                for(j=0;j<m;j++)
+                {
+                    if(r[j]<=ne[k][j])//if request <=need
+                    {
+                        if(r[j]<=av[j])//if request<=available
+                            {
+                                av[j]=av[j]-r[j];
+                                a[k][j]=a[k][j]+r[j];
+                                ne[k][j]=ne[k][j]-r[j];
+                                
+                                
+                            }
+                        else
+                            printf("resources are not available\n");
+                    }
+                    else
+                         printf("request exceeds claim\n");
+                }
+                f=safe ( a, ma, av, ne);//go to safety algorithm
+            }
+       }
+    printf("\n");
+    
+      if(f=1)//resources are allocated
+        printf("request is granted\n");
+    else
+            printf("not granted");//the request is not permitted.because there is a chance for deadlock.
+}
+
+
+
+
+
+
