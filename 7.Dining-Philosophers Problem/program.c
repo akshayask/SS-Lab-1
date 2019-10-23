a7fab3f479a2ab4bfc1b8131255a6cbde505d31f
Ajil <39672981+Ajil5467@users.noreply.github.com>
2019-10-23 21:46:11 +0530

    1
    1

--- 7.Dining-Philosophers Problem/program.c
+++ 7.Dining-Philosophers Problem/program.c
@@ -59,7 +59,7 @@ void test(int ph_num) //check for the availability of forks
     state[ph_num] = EATING;
     sleep(2);
     printf("Philosopher %d takes fork %d and %
-      d\ n ",ph_num+1,LEFT+1,ph_num+1);
+      d\n ",ph_num+1,LEFT+1,ph_num+1);
       printf("Philosopher %d is Eating\n", ph_num + 1); sem_post( & S[ph_num]);
     }
   }
@@ -67,6 +67,6 @@ void test(int ph_num) //check for the availability of forks
       sem_wait( & mutex);
       state[ph_num] = THINKING;
       printf("Philosopher %d putting fork %d and %d
-        down\ n ",ph_num+1,LEFT+1,ph_num+1); //after finished eating , both
+        down\n ",ph_num+1,LEFT+1,ph_num+1); //after finished eating , both
         forks put down printf("Philosopher %d is thinking\n", ph_num + 1); test(LEFT); test(RIGHT); sem_post( & mutex);
       }
